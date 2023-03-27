60fbad78293aef9cfee9dc270c956e889bc9d34e
Pedro Henrique A <phaalmeida1@gmail.com>
2023-03-27 00:33:29 -0300

    Testes da classe Campo terminados
    Finalização dos testes da classe Campo, completando e cobrindo mais partes do codigo da classe.

--- test/linkedy/com/github/cm/modelo/CampoTeste.java
+++ test/linkedy/com/github/cm/modelo/CampoTeste.java
@@ -1,5 +1,6 @@
 package linkedy.com.github.cm.modelo;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -151,6 +152,109 @@ public class CampoTeste {
 		campo.abrir();
 		
 		assertTrue(vizinho1.isAberto() && vizinhoDoVizinho1.isFechado());
+	}
+	
+	@Test
+	void testeVizinhancaSegura() {
+		Campo vizinho = new Campo(3, 4);
+		campo.adicionarVizinho(vizinho);
+		
+		assertTrue(campo.vizinhancaSegura());
+	}
+	
+	@Test
+	void testeVizinhancaNaoSegura() {
+		Campo vizinho = new Campo(3, 4);
+		vizinho.minar();
+		campo.adicionarVizinho(vizinho);
+		
+		assertFalse(campo.vizinhancaSegura());
+	}
+	
+	@Test
+	void testeObjetivoAlcancado1() {
+		campo.abrir();
+		
+		assertTrue(campo.objetivoAlcancado());
+	}
+	
+	@Test
+	void testeObjetivoAlcancado2() {
+		campo.minar();
+		campo.alternarMarcacao();
+		
+		assertTrue(campo.objetivoAlcancado());
+	}
+	
+	@Test
+	void testeObjetivoNaoAlcancado() {
+		assertFalse(campo.objetivoAlcancado());
+	}
+	
+	@Test
+	void testeMinasNaVizinhanca1() {
+		Campo vizinho = new Campo(3, 4);
+		vizinho.minar();
+		campo.adicionarVizinho(vizinho);
+		
+		assertEquals(1, campo.minasNaVizinhanca());
+	}
+	
+	@Test
+	void testeMinasNaVizinhanca2() {
+		Campo vizinho1 = new Campo(3, 4);
+		vizinho1.minar();
+		Campo vizinho2 = new Campo(2, 4);
+		vizinho2.minar();
+		campo.adicionarVizinho(vizinho1);
+		campo.adicionarVizinho(vizinho2);
+		
+		assertEquals(2, campo.minasNaVizinhanca());
+	}
+	
+	@Test
+	void testeReiniciarCampo() {
+		campo.alternarMarcacao();
+		campo.reiniciar();
+		
+		assertTrue(campo.isFechado() && !campo.isMarcado());
+	}
+	
+	@Test
+	void testeGeters() {
+		boolean linhaIgual = 3 == campo.getLinha();
+		boolean colunaIgual = 3 == campo.getColuna();
+		
+		assertTrue(colunaIgual && linhaIgual);
+	}
+	
+	@Test
+	void testeToString1() {
+		campo.alternarMarcacao();
+		assertEquals("x", campo.toString());
+	}
+	
+	@Test
+	void testeToString2() {
+		Campo vizinho = new Campo(3, 4);
+		vizinho.minar();
+		campo.adicionarVizinho(vizinho);
+		campo.abrir();
+		assertEquals("1", campo.toString());
+	}
+	
+	@Test
+	void testeToString3() {
+		campo.minar();
+		assertThrows(ExplosaoException.class, () -> {
+			campo.abrir();
+		});
+		assertEquals("*", campo.toString());
+	}
+	
+	@Test
+	void testeToString4() {
+		assertEquals("?", campo.toString());
 	}
 	
 }
