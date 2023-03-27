e1cdb54edd7660109d093763c26204f9b7ecb884
Pedro Henrique A <phaalmeida1@gmail.com>
2023-03-27 17:42:48 -0300

    Jogabilidade Adicionada
    Agora é possivel jogar de verdade o  jogo, ao iniciar o arquivo Aplicacao, o jogo começara, para escolher um campo para abri-lo ou marca-lo basta digitar suas cordenadas (x, y) que logo apos você podera decidir oque fazer com ele

--- src/linkedy/com/github/cm/Aplicacao.java
+++ src/linkedy/com/github/cm/Aplicacao.java
@@ -1,14 +1,12 @@
 package linkedy.com.github.cm;
 
 import linkedy.com.github.cm.modelo.Tabuleiro;
+import linkedy.com.github.cm.visao.TabuleiroConsole;
 
 public class Aplicacao {
 	public static void main(String[] args) {
-		Tabuleiro tabuleiro = new Tabuleiro(5, 5, 5);
+		Tabuleiro tabuleiro = new Tabuleiro(5, 5, 2);
 		
-		tabuleiro.abrir(2, 2);
-		tabuleiro.alternarMarcacao(3, 3);
-		
-		System.out.println(tabuleiro);
+		new TabuleiroConsole(tabuleiro);
 	}
 }
---
+++ src/linkedy/com/github/cm/visao/TabuleiroConsole.java
@@ -0,0 +1,79 @@
+package linkedy.com.github.cm.visao;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.Scanner;
+
+import linkedy.com.github.cm.excecao.ExplosaoException;
+import linkedy.com.github.cm.excecao.SairException;
+import linkedy.com.github.cm.modelo.Tabuleiro;
+
+public class TabuleiroConsole {
+	private Tabuleiro tabuleiro;
+	private Scanner entradaUsuario = new Scanner(System.in);
+	
+	public TabuleiroConsole(Tabuleiro tabuleiro) {
+		this.tabuleiro = tabuleiro;
+		
+		executarJogo();
+	}
+
+	private void executarJogo() {
+		try {
+			boolean continuar = true;
+			
+			while (continuar) {
+				cicloDoJogo();
+				
+				System.out.println("Outra partida? (S/n): ");
+				String resposta = entradaUsuario.nextLine();
+				if("n".equalsIgnoreCase(resposta)) {
+					continuar = false;
+				} else {
+					tabuleiro.reiniciar();
+				}
+			}
+		} catch (SairException e) { 
+			System.out.println("Jogo Encerrado!!!");
+		} finally {
+			entradaUsuario.close();
+		}
+		
+	}
+
+	private void cicloDoJogo() {
+		try {
+			
+			while (!tabuleiro.objetivoAlcancado()) {
+				System.out.println(tabuleiro);
+				
+				String respostaCordenada = capturarRespostaUsuario("Digite (x, y): ");
+				
+				Iterator<Integer> xy = Arrays.stream(respostaCordenada.split(",")).map(n -> Integer.parseInt(n.trim())).iterator();
+				
+				String respostaAbrirOuMarca = capturarRespostaUsuario("Digite 1 - Abrir ou 2 - (Des)Marcar: ");
+				
+				if ("1".equalsIgnoreCase(respostaAbrirOuMarca)) {
+					tabuleiro.abrir(xy.next(), xy.next());
+				} else if ("2".equalsIgnoreCase(respostaAbrirOuMarca)) {
+					tabuleiro.alternarMarcacao(xy.next(), xy.next());
+				}
+			}
+			
+			System.out.println("Você ganhou!!");
+		} catch (ExplosaoException e) {
+			System.out.println("Você perdeu!!");
+		}
+	}
+	
+	private String capturarRespostaUsuario(String texto) {
+		System.out.print(texto);
+		String resposta = entradaUsuario.nextLine();
+		
+		if ("sair".equalsIgnoreCase(resposta)) {
+			throw new SairException();
+		}
+		
+		return resposta;
+	}
+}
